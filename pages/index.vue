faf89d1d4f1a76d852ed38c1b2d5d5bd7c657d71
Madeee1 <madeee@duck.com>
2024-04-20 01:18:46 +0000

    feat: tailwind and example page

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="flex flex-col w-[300px] place-content-center mx-auto">
+        <h1 class="text-3xl mx-auto">{{ count }}</h1>
+        <button class="border-2 border-black" @click="increment">Increment</button>
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        data() {
+            return {
+                count: 0
+            }
+        },
+        methods: {
+            increment() {
+                this.count++
+            }
+        }
+    };
+</script>
