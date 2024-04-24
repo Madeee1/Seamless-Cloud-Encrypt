5611acbc7399dec336fe74d17adf048d55c3a26c
Madeee1 <madeee@duck.com>
2024-04-24 07:52:35 +0000

    Revert "Merge branch 'feat/nichlas'"
    This reverts commit fb7c6bf79fee8ca4b0422442caf5c93710bcd676, reversing
    changes made to be227f60ec08cb7d137e3772ab7dfd7d57a6a9bf.

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,21 @@
 <template>
-    <div>
-        <h1>This is main page, go to /example for example</h1>
+    <div class="flex flex-col w-[300px] place-content-center mx-auto">
+        <h1 class="text-3xl mx-auto">{{ count }}</h1>
+        <button class="border-2 border-black" @click="increment">Increment</button>
     </div>
 </template>
+
+<script lang="ts">
+    export default {
+        data() {
+            return {
+                count: 0
+            }
+        },
+        methods: {
+            increment() {
+                this.count++
+            }
+        }
+    };
+</script>
