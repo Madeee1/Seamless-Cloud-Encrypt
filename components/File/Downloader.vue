b22dcc533331ac6d8741d78f3f355b585d72260c
nicolaschen2293 <147520907+nicolaschen2293@users.noreply.github.com>
2024-05-24 00:51:55 +0000

    uploader and downloader

--- components/File/Downloader.vue
+++ components/File/Downloader.vue
@@ -1,9 +1,96 @@
 <template>
   <div>
-    
+    <br>
+    <br>
+    <p>Upload files you want to DECRYPT here</p>
+    <input
+      ref="fileInput"
+      type="file"
+      @change="handleFileUpload"
+    >
+    <form>
+      <label for="keyPass">Password: </label>
+      <input
+        v-model="keyPass"
+        type="text"
+      >
+      <label for="iv">IV: </label>
+      <input
+        v-model="iv"
+        type="text"
+      >
+    </form>
+    <div>
+      <p>You can download the decrypted file over here</p>
+      <a
+        :href="decryptedFileURL"
+        download="decryptedFile.txt"
+      >Download</a>
+      <p>{{ decryptedFileURL }}</p>
+    </div>
   </div>
 </template>
 
-<script lang="ts" setup>
+<script>
+export default {
+  data() {
+    return {
+      JWK: { test: 'test' },
+      decryptedFileURL: '',
+      keyPass: '',
+      hardIV: '',
+    }
+  },
+  computed: {
+    imagePreview() {
+      return this.$refs.fileInput.files[0]
+    },
+  },
+  methods: {
+    async handleFileUpload() {
+      const password = this.keyPass
+      const salt = new Uint8Array([1, 2, 3, 4])
+      const file = this.$refs.fileInput.files[0]
+      const cryptoKeyObj = await this.deriveKeyFromPassword(password, salt)
+      const decryptedFile = await this.decrypt(file, cryptoKeyObj)
 
+      // Encrypted file is ArrayBuffer type, needs to be blob to be downloaded
+      this.decryptedFileURL = URL.createObjectURL(decryptedFile)
+    },
+    async decrypt(encrypted, key) {
+      const iv = this.hardIV
+      const decrypted = await crypto.subtle.decrypt({ name: 'AES-GCM', iv: iv }, key, encrypted)
+
+      const decoded = new TextDecoder().decode(decrypted)
+
+      return decoded
+    },
+    async deriveKeyFromPassword(password, salt) {
+      const encoder = new TextEncoder()
+      const encodedPassword = encoder.encode(password)
+      const derivedKey = await crypto.subtle.importKey(
+        'raw',
+        encodedPassword,
+        { name: 'PBKDF2' },
+        false,
+        ['deriveKey'],
+      )
+
+      const key = await crypto.subtle.deriveKey(
+        {
+          name: 'PBKDF2',
+          salt: salt,
+          iterations: 100000, // You can adjust this based on your security requirements
+          hash: 'SHA-256',
+        },
+        derivedKey,
+        { name: 'AES-GCM', length: 256 }, // 256-bit key for AES-GCM
+        false,
+        ['encrypt', 'decrypt'],
+      )
+
+      return key
+    },
+  },
+}
 </script>
--- components/File/Uploader.vue
+++ components/File/Uploader.vue
@@ -9,6 +9,12 @@
       >
     </div>
     <div>
+      <form>
+        <label for="keyPass">Password: </label>
+        <input v-model="keyPass" type="text">
+      </form>
+    </div>
+    <div>
       <p>Generate JWK Key</p>
       <button @click="generateKey()">
         Generate Key
@@ -24,6 +30,17 @@
         download="encrypted-file.bin"
       >Download</a>
       <p>{{ encryptedFileURL }}</p>
+      <br>
+      <br>
+    </div>
+    <div>
+      <p>You can download the DECRYPTED file over here</p>
+      <a
+        :href="decryptedFileURL"
+        download="ori-file.txt"
+      >Download&Decrypt</a>
+      <p>{{ decryptedFileURL }}</p>
+      <br>
     </div>
   </div>
 </template>
@@ -34,6 +51,9 @@ export default {
     return {
       JWK: { test: 'test' },
       encryptedFileURL: '',
+      decryptedFileURL: '',
+      keyPass: '',
+      initVector: '',
     }
   },
   computed: {
@@ -42,16 +62,64 @@ export default {
     },
   },
   methods: {
+    async deriveKeyFromPassword(password, salt) {
+      const encoder = new TextEncoder()
+      const encodedPassword = encoder.encode(password)
+      const derivedKey = await crypto.subtle.importKey(
+        'raw',
+        encodedPassword,
+        { name: 'PBKDF2' },
+        false,
+        ['deriveKey'],
+      )
+
+      const key = await crypto.subtle.deriveKey(
+        {
+          name: 'PBKDF2',
+          salt: salt,
+          iterations: 100000, // You can adjust this based on your security requirements
+          hash: 'SHA-256',
+        },
+        derivedKey,
+        { name: 'AES-GCM', length: 256 }, // 256-bit key for AES-GCM
+        false,
+        ['encrypt', 'decrypt'],
+      )
+
+      return key
+    },
+    async printPass() {
+      const password = this.keyPass
+      const salt = new Uint8Array([1, 2, 3, 4]) // You should generate a random salt
+      this.deriveKeyFromPassword(password, salt)
+        .then(key => {
+          console.log('Derived key:', key)
+        // Key successfully derived, you can proceed with encryption/decryption
+        })
+        .catch(error => {
+          console.error('Error deriving key:', error)
+        // Key derivation failed, handle the error
+        })
+    },
     async handleFileUpload() {
+      const password = this.keyPass
+      const salt = new Uint8Array([1, 2, 3, 4])
       const file = this.$refs.fileInput.files[0]
-      const cryptoKeyObj = await this.generateKey()
+      const cryptoKeyObj = await this.deriveKeyFromPassword(password, salt)
       const ivAndEncrypted = await this.encrypt(file, cryptoKeyObj)
 
       // Encrypted file is ArrayBuffer type, needs to be blob to be downloaded
+      console.log('iv for this file: ', ivAndEncrypted.iv)
+      this.initVector = ivAndEncrypted.iv
       const blobEncryptedFile = new Blob([ivAndEncrypted.encrypted], {
         type: 'application/octet-stream',
       })
       this.encryptedFileURL = URL.createObjectURL(blobEncryptedFile)
+
+      const decryptedFile = await this.decrypt(ivAndEncrypted.encrypted, ivAndEncrypted.iv, cryptoKeyObj)
+      const blobDecryptedFile = new Blob([decryptedFile], { type: 'text/plain' })
+
+      this.decryptedFileURL = URL.createObjectURL(blobDecryptedFile)
     },
     async generateKey() {
       const returnValue = await crypto.subtle.generateKey(
@@ -96,15 +164,10 @@ export default {
       )
       return { iv, encrypted }
     },
-    async decrypt(encrypted, key) {
-      return await crypto.subtle.decrypt(
-        {
-          name: 'AES-GCM',
-          iv: encrypted.iv,
-        },
-        key,
-        encrypted.encrypted,
-      )
+    async decrypt(encrypted, iv, key) {
+      const decrypted = await crypto.subtle.decrypt({ 'name': 'AES-GCM', 'iv': iv }, key, encrypted)
+      const decoded = new TextDecoder().decode(decrypted)
+      return decoded
     },
   },
 }
