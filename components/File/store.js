95038146974967a2423be4d910ffc7db58092ce2
nicolaschen2293 <147520907+nicolaschen2293@users.noreply.github.com>
2024-05-28 10:02:25 +0000

    pinia store for key, iv inside encryptde file, separated encrypt-decrypt

---
+++ components/File/store.js
@@ -0,0 +1,13 @@
+import { defineStore } from 'pinia'
+
+export const useObjectStore = defineStore({
+  id: 'object',
+  state: () => ({
+    object: null,
+  }),
+  mutations: {
+    setObject(newObject) {
+      this.object = newObject
+    },
+  },
+})
