5a94226281f4e07bb5eb752dc798837ca3d1a48b
nicolaschen2293 <147520907+nicolaschen2293@users.noreply.github.com>
2024-05-30 01:25:57 +0000

    fix: initialized pinia in pages/dashboard/index.vue

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -6,5 +6,10 @@
 </template>
 
 <script setup>
+import { createPinia } from 'pinia'
+import { defineComponent, createApp } from 'vue'
 
+const pinia = createPinia()
+const app = createApp(defineComponent({}))
+app.use(pinia)
 </script>
