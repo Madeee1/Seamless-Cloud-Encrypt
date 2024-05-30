733cf97f716ca6e5aa0cdcfa7ed8cda88e8ecdd5
NicholasS <117060824+NicholasS2218@users.noreply.github.com>
2024-05-30 14:42:48 +0000

    for vault

--- components/Vault/Create.vue
+++ components/Vault/Create.vue
@@ -1,9 +1,44 @@
 <template>
-  <div>
-    
-  </div>
-</template>
-
-<script lang="ts" setup>
-
-</script>
+    <div>
+      <form>
+        <p>Create Vault</p>
+        <label>Name</label>
+        <input type="text" required v-model="vaultName">
+        <br>
+        <label>Password</label>
+        <input type="password" required v-model="vaultPassword">
+        <br>
+        <label>Cloud</label>
+        <select v-model="vaultCloud">
+          <option value="None">None</option>
+          <option value="OneDrive">OneDrive</option>
+          <option value="GDrive">Google Drive</option>
+        </select>
+        <br>
+      </form>
+      <div>
+        <button @click="createVault()">Create Vault</button>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  const supabase = useSupabaseClient()
+  const user = useSupabaseUser() 
+  
+  const vaultName = ref('')
+  const vaultPassword = ref('')
+  const vaultCloud = ref('')
+  
+  async function createVault(){
+    if (vaultCloud.value == "None")
+    {
+        const { data, error } = await supabase.from('vaultstest')
+        .insert({'vaultName': vaultName.value, 'vaultPassword': vaultPassword.value})
+        .select()
+    }
+    const { data, error } = await supabase.from('vaultstest')
+    .insert({'vaultName': vaultName.value, 'vaultPassword': vaultPassword.value, 'cloudProvider': vaultCloud.value})
+    .select()
+  }
+  </script>
