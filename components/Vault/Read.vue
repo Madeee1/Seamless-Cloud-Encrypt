311b242cb0b685d958fb63c97092042cbd88c73d
NicholasS <117060824+NicholasS2218@users.noreply.github.com>
2024-06-08 15:26:05 +0000

    update vault rud

--- components/Vault/Read.vue
+++ components/Vault/Read.vue
@@ -1,23 +1,16 @@
 <template>
     <div>
-        <button @click="readVault()">Read Vault</button>
+        <button @click="readVault()" class="border border-black">Read Vault</button>
     </div>
     <div>
         <h1>Fetched Data</h1>
         <dl v-for="vault in vaults"
           :key="vault.id">
           <div>
-            <dt>Vault ID: {{ vault.id }}</dt>
-            <dd>Vault Name: {{ vault.name }}</dd>
-            <dd>Created At: {{ vault.created_at }}</dd>
-            <dd>User ID: {{ vault.user_id }}</dd>
-            <dd>Cloud Folder: {{ vault.cloud_folder_name }}</dd>
-            <dd>Access Token: {{ vault.enc_cloud_access_token }}</dd>
-            <dd>Refresh Token: {{ vault.enc_cloud_refresh_token }}</dd>
-            <dd>Cloud: {{ vault.cloud_provider }}</dd>
-            <dd>Hashed Password: {{ vault.hashed_password }}</dd>
-            <dd>Vault Description:{{ vault.description }}</dd>
-            <dd>Idle Time: {{ vault.idle_time }}</dd>
+            <dd>Vault Name: {{ vault.name }}
+            <button @click="updateVault( vault.id )" class="border border-black">Update</button>
+            <button @click="deleteVault( vault.id )" class="border border-black">Delete</button>
+            </dd>
             <br>
           </div>
         </dl>
@@ -27,10 +20,16 @@
 const supabase = useSupabaseClient()
 const vaults = ref('')
 async function readVault(){
-  const {data, error} =  await supabase.from('vault')
+  const {data: vault, error} =  await supabase.from('vault')
   .select()
 
-  vaults.value = data
-  return vaults
+  vaults.value = vault
+}
+async function updateVault(id){
+  localStorage.setItem('vaultId', id )
+  navigateTo('/updatePage')
+}
+async function deleteVault(id){
+  const { error } = await supabase.from('vault').delete().eq('id', id)
 }
 </script>
