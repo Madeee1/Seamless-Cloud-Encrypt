46f055381ce2cbb1cef53cde2238f86f22a950fa
Madeee1 <madeee@duck.com>
2024-06-11 06:17:43 +0000

    feat: dashboard layout

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -12,4 +12,10 @@ import { defineComponent, createApp } from 'vue'
 const pinia = createPinia()
 const app = createApp(defineComponent({}))
 app.use(pinia)
+
+definePageMeta({
+  title: 'Dashboard',
+  description: 'Dashboard page',
+  layout: 'dashboard',
+})
 </script>
