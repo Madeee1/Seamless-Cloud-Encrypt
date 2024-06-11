00b0e4388807dcf42638aba2245b9a53335d7697
Madeee1 <madeee@duck.com>
2024-06-11 05:49:19 +0000

    feat: init vault crud

--- components/Vault/Read.vue
+++ components/Vault/Read.vue
@@ -1,35 +1,44 @@
 <template>
+  <div>
     <div>
-        <button @click="readVault()" class="border border-black">Read Vault</button>
+      <h1 class="text-xl font-bold">Fetched Data</h1>
+      <dl v-for="vault in vaults" :key="vault.id" class="mt-4">
+        <div class="flex items-center">
+          <dd class="mr-4">Vault Name: {{ vault.name }}</dd>
+          <button
+            class="border border-black px-4 py-2"
+            @click="updateVault(vault.id)"
+          >
+            Update
+          </button>
+          <button
+            class="border border-black px-4 py-2"
+            @click="deleteVault(vault.id)"
+          >
+            Delete
+          </button>
+        </div>
+      </dl>
     </div>
-    <div>
-        <h1>Fetched Data</h1>
-        <dl v-for="vault in vaults"
-          :key="vault.id">
-          <div>
-            <dd>Vault Name: {{ vault.name }}
-            <button @click="updateVault( vault.id )" class="border border-black">Update</button>
-            <button @click="deleteVault( vault.id )" class="border border-black">Delete</button>
-            </dd>
-            <br>
-          </div>
-        </dl>
-    </div>
-  </template>
+  </div>
+</template>
 <script setup>
 const supabase = useSupabaseClient()
 const vaults = ref('')
-async function readVault(){
-  const {data: vault, error} =  await supabase.from('vault')
-  .select()
+async function readVault() {
+  const { data: vault, error } = await supabase.from('vault').select()
 
   vaults.value = vault
 }
-async function updateVault(id){
-  localStorage.setItem('vaultId', id )
+onMounted(() => {
+  readVault()
+})
+
+async function updateVault(id) {
+  localStorage.setItem('vaultId', id)
   navigateTo('/updatePage')
 }
-async function deleteVault(id){
+async function deleteVault(id) {
   const { error } = await supabase.from('vault').delete().eq('id', id)
 }
 </script>
---
+++ pages/vault.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex flex-col w-[500px] place-content-center mx-auto">
+    <h1>VAULT CREATE:</h1>
+    <VaultCreate />
+    <br />
+    <h1>VAULT UPDATE:</h1>
+    <h1>VAULT DELETE:</h1>
+    <VaultDelete />
+    <br />
+    <h1>VAULT READ:</h1>
+    <VaultRead />
+  </div>
+</template>
+  
