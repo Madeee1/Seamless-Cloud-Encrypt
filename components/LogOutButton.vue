46e541314334250d48d41a3c279ff975f492f534
Madeee1 <madeee@duck.com>
2024-06-11 07:21:42 +0000

    feat: log out button has modal confirmation

--- components/LogOutButton.vue
+++ components/LogOutButton.vue
@@ -1,19 +1,34 @@
 <template>
   <div>
-    <UButton
-      color="red"
-      outlined
-      @click="logout"
-    >
-      Log out
-    </UButton>
+    <UButton color="red" outlined @click="openModal"> Log out </UButton>
+    <UModal v-model="isModalOpen">
+      <div>
+        <UCard>
+          <template #header>
+            <h1 class="text-xl font-bold">Are you sure you want to log out?</h1>
+          </template>
+
+          <div class="flex gap-4">
+            <UButton color="red" @click="logout">Yes</UButton>
+            <UButton @click="isModalOpen = false">No</UButton>
+          </div>
+        </UCard>
+      </div>
+    </UModal>
   </div>
 </template>
 
 <script lang="ts" setup>
+const isModalOpen = ref(false)
+
+function openModal() {
+  isModalOpen.value = true
+}
+
 const supabase = useSupabaseClient()
 function logout() {
   supabase.auth.signOut()
+  navigateTo('/')
 }
 </script>
 
