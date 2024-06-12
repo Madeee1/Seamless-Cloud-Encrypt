1c517eca1e5e553b35d26f638a20a3a61137ce7f
Madeee1 <madeee@duck.com>
2024-06-12 07:46:23 +0000

    fix: pojo error

--- stores/allVault.js
+++ stores/allVault.js
@@ -4,24 +4,8 @@ import { defineStore } from 'pinia'
 export const useAllVaultStore = defineStore({
   id: 'myAllVaultStore',
   state: () => ({
-    supabase: useSupabaseClient(),
-    user: useSupabaseUser(),
+    loading: false,
     vaults: [],
   }),
-  actions: {
-    // DEPRECATED
-    async update() {
-      const { data: vault, error } = await supabase
-        .from('vault')
-        .select('id, name')
-        .eq('user_id', user.value.id)
-
-      if (error) {
-        console.error('Error fetching vaults:', error.message)
-        return
-      } else {
-        this.vaults = vault
-      }
-    },
-  },
+  actions: {},
 })
