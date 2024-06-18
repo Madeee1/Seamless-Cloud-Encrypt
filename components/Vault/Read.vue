f2c45a49250088a05c75ddb27f019429ea506382
nicolaschen2293 <147520907+nicolaschen2293@users.noreply.github.com>
2024-06-18 02:02:07 +0000

    feat: added description for vaults

--- components/Vault/Read.vue
+++ components/Vault/Read.vue
@@ -4,7 +4,7 @@
       <div
         class="flex items-center border border-gray-300 p-4 rounded-lg shadow-sm hover:shadow-md hover:border-gray-400 hover:bg-gray-50 focus:shadow-outline transition-all duration-150 ease-in-out cursor-pointer"
         tabindex="0"
-        @click="openVault(vault.id, vault.name)"
+        @click="openVault(vault.id, vault.name, vault.description)"
       >
         <UIcon name="i-heroicons-lock-closed" class="text-gray-700" />
         <dd class="ml-4 flex-1 text-gray-900">{{ vault.name }}</dd>
@@ -22,7 +22,7 @@ async function readVault() {
   allVaults.pending = true
   const { data: vault, error } = await supabase
     .from('vault')
-    .select('id, name')
+    .select('id, name, description')
     .eq('user_id', user.value.id)
 
   allVaults.vaults = vault
@@ -33,9 +33,10 @@ onMounted(() => {
   readVault()
 })
 
-function openVault(id, name) {
+function openVault(id, name, description) {
   vaultPinia.id = id
   vaultPinia.name = name
+  vaultPinia.description = description
   navigateTo('/dashboard/open')
 }
 </script>
