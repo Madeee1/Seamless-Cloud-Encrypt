a23f78bb76d9e9e8c365a4a8d36a4cb6877a113f
Madeee1 <madeee@duck.com>
2024-06-18 06:38:08 +0000

    feat: ui changed after merge

--- components/Vault/Update.vue
+++ components/Vault/Update.vue
@@ -1,27 +1,35 @@
 <template>
-  <div>
-    <h1 class="text-2xl font-bold">Update Vault:</h1>
-    <div>
-      <form>
-        <div>
-          <label class="block mt-4">Name:</label>
-          <input
-            v-model="updatedVaultName"
-            type="text"
-            required
-            class="border border-black p-2"
-          />
-        </div>
-        <div>
-          <label class="block mt-4">Description:</label>
-          <textarea
-            v-model="updatedVaultDescription"
-            class="border border-black p-2 w-[600px] h-[200px]"
-          />
-        </div>
-        <UButton class="mx-4 mt-4" @click="updateVault()">Update Vault</UButton>
-      </form>
-    </div>
+  <div class="max-w-lg mx-auto my-8">
+    <h1 class="text-2xl font-bold text-center mb-6">Update Vault:</h1>
+    <form>
+      <div class="mb-4">
+        <label class="block text-gray-700 text-sm font-bold mb-2">Name:</label>
+        <input
+          v-model="updatedVaultName"
+          type="text"
+          required
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        />
+      </div>
+      <div class="mb-6">
+        <label class="block text-gray-700 text-sm font-bold mb-2"
+          >Description:</label
+        >
+        <textarea
+          v-model="updatedVaultDescription"
+          required
+          class="w-[600px] shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          rows="5"
+        ></textarea>
+      </div>
+      <div class="flex justify-center">
+        <UButton
+          class="font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          @click="updateVault()"
+          >Update Vault</UButton
+        >
+      </div>
+    </form>
   </div>
 </template>
 
