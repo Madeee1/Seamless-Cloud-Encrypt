7a87679f32e1ca514b15ca96d651ca8b060200d3
Madeee1 <madeee@duck.com>
2024-06-19 03:45:27 +0000

    fix: missing function call to update account

--- components/Account/View.vue
+++ components/Account/View.vue
@@ -12,7 +12,9 @@
             type="text"
             class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           />
-          <UButton class="mt-4"> Update Email </UButton>
+          <UButton class="mt-4" @click="updateAccountEmail">
+            Update Email
+          </UButton>
         </div>
         <div class="mb-6">
           <label class="block text-gray-700 text-sm font-bold mb-2"
@@ -23,7 +25,9 @@
             type="password"
             class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
           />
-          <UButton class="mt-4"> Update Password </UButton>
+          <UButton class="mt-4" @click="updateAccountPass">
+            Update Password
+          </UButton>
         </div>
       </form>
     </div>
