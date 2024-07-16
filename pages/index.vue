13faadadac584ec30ef664cce14bbe513e20bfac
NicholasS <117060824+NicholasS2218@users.noreply.github.com>
2024-07-16 05:36:38 +0000

    landing page version 1

--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,79 @@
 <template>
-  <div
-    class="flex flex-col items-center justify-center min-h-screen bg-gradient-to-t from-gray-900 to-blue-400 text-white"
-  >
+  <div class="flex flex-col min-h-screen bg-main-blue text-gray-200">
+    <link
+      href="https://fonts.googleapis.com/css2?family=Anton&family=Heebo:wght@100..900&display=swap"
+      rel="stylesheet"
+    />
+    <!--
     <div class="mb-8">
       <img src="" alt="Crypt and Go Logo" class="h-16" />
     </div>
-    <h1 class="text-5xl font-extrabold mb-4 text-center">
-      Secure Your Files with Client-Side Encryption
-    </h1>
-    <p class="text-lg text-center mb-8 max-w-lg">
-      Trust no one but yourself with your data. Encrypt your files locally
-      before uploading them to your preferred cloud service. Secure, seamless,
-      and simple.
-    </p>
-    <div class="flex space-x-4">
-      <button
-        class="bg-lime-500 hover:bg-lime-600 text-white px-6 py-3 rounded-md font-semibold"
-        @click="navigateTo('/login')"
+    -->
+    <section class="h-min">
+      <h1 class="text-5xl font-extrabold mb-5 mt-20 text-center">
+        Welcome to
+        <span class="text-third-blue font-title"
+          >CRYPT<span class="text-gray-200 text-4xl">&</span>GO</span
+        >
+        <!-- for later use
+        <span
+          class="text-transparent text-stroke-white text-stroke-1 font-sans"
+          >and</span
+        -->
+      </h1>
+      <div
+        class="h-16 w-3/4 bg-third-blue rounded-md transform- -skew-x-12 mx-auto shadow-[10px_5px_0px_rgb(255,0,0)]"
       >
-        Get Started
-      </button>
-      <button
-        class="bg-white text-gray-700 hover:text-gray-900 px-6 py-3 rounded-md border border-gray-300 font-semibold"
-        @click="moveToDashboard"
+        <h2
+          class="text-4xl font-extrabold w-3/4 mx-auto pt-2 transform skew-x-12 text-center text-white"
+        >
+          Secure Your Files with Client-Side Encryption
+        </h2>
+      </div>
+      <p class="text-xl text-gray-200 text-center mb-5 max-w-xl mt-10 mx-auto">
+        Trust no one but yourself with your data. Encrypt your files locally
+        before uploading them to your preferred cloud service. Secure, seamless,
+        and simple.
+      </p>
+      <div class="flex space-x-4 w-1/3 mx-auto text-lg mt-10">
+        <button
+          class="flex-1 bg-third-blue text-gray-200 px-6 py-3 rounded-md font-semibold hover:shadow-[5px_3px_0px_rgb(255,0,0)] hover:bg-white hover:text-third-blue"
+          @click="navigateTo('/login')"
+        >
+          Get Started
+        </button>
+        <button
+          class="flex-1 bg-white text-third-blue px-6 py-3 rounded-md font-semibold hover:shadow-[5px_3px_0px_rgb(255,0,0)] hover:bg-third-blue hover:text-gray-200"
+          @click="moveToDashboard"
+        >
+          Open Dashboard
+        </button>
+      </div>
+      <div class="flex items-center justify-center mt-10 space-x-8">
+        <div class="flex items-center space-x-2">
+          <span class="text-lime-400">&#10003;</span>
+          <p class="text-lg">Free forever for core features</p>
+        </div>
+        <div class="flex items-center space-x-2">
+          <span class="text-lime-400">&#10003;</span>
+          <p class="text-lg">More integrations than any other platform</p>
+        </div>
+      </div>
+    </section>
+    <div
+      class="aspect-900/200 w-full bg-repeat bg-center bg-cover bg-wave2"
+    ></div>
+    <section class="h-screen bg-third-blue">
+      <div
+        class="h-16 w-3/4 bg-second-blue rounded-md transform- -skew-x-12 mx-auto shadow-[10px_5px_0px_rgb(255,0,0)]"
       >
-        Open Dashboard
-      </button>
-    </div>
-    <div class="flex items-center justify-center mt-12 space-x-8">
-      <div class="flex items-center space-x-2">
-        <span class="text-lime-400">&#10003;</span>
-        <p class="text-lg">Free forever for core features</p>
+        <h2
+          class="text-4xl font-extrabold w-3/4 mx-auto pt-2 transform skew-x-12 text-center text-first-blue"
+        >
+          Features
+        </h2>
       </div>
-      <div class="flex items-center space-x-2">
-        <span class="text-lime-400">&#10003;</span>
-        <p class="text-lg">More integrations than any other platform</p>
-      </div>
-    </div>
+    </section>
   </div>
 </template>
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,68 @@
+import { _width } from '#tailwind-config/theme'
 import type { Config } from 'tailwindcss'
 
 export default {
   content: [],
   theme: {
     extend: {
+      backgroundColor: {
+        'main-blue': '#00041e',
+        'second-blue': '#001FD2',
+        'third-blue': '#0FA2FF',
+      },
+      colors: {
+        'main-blue': '#00041e',
+        'second-blue': '#001FD2',
+        'third-blue': '#0FA2FF',
+      },
       fontFamily: {
         sans: ['Lato', 'sans-serif'],
+        title: ['Anton'],
+        title2: ['Montserrat'],
+      },
+      textStrokeWidth: {
+        '1': '0.1px',
+      },
+      textStrokeColor: {
+        black: '#000',
+        white: '#fff',
+        'main-blue': '#00041e',
+        'second-blue': '#001FD2',
+        'third-blue': '#0FA2FF',
+      },
+      aspectRatio: {
+        '960/300': '960 / 300',
+        '900/200': '900 / 200',
+      },
+      backgroundImage: {
+        wave1: "url('/img/wave1.svg')",
+        wave2: "url('/img/wave2.svg')",
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities, theme, e }) {
+      const textStrokeWidth = theme('textStrokeWidth', {})
+      const textStrokeColor = theme('textStrokeColor', {})
+
+      const utilities = {}
+
+      for (const key in textStrokeWidth) {
+        utilities[`.${e(`text-stroke-${key}`)}`] = {
+          WebkitTextStrokeWidth: textStrokeWidth[key],
+          textStrokeWidth: textStrokeWidth[key],
+        }
+      }
+
+      for (const key in textStrokeColor) {
+        const value = textStrokeColor[key]
+
+        utilities[`.${e(`text-stroke-${key}`)}`] = {
+          WebkitTextStrokeColor: value,
+          textStrokeColor: value,
+        }
+      }
+      addUtilities(utilities, ['responsive', 'hover'])
+    },
+  ],
 } satisfies Config
