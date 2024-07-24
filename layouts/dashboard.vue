a1dcee8f6c904900469aceb69d7820be547c6c75
Madeee1 <madeee@duck.com>
2024-07-24 05:59:11 +0000

    feat: fix duplicate component issue

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -1,38 +1,40 @@
 <template>
-  <body class="h-screen flex flex-col">
-    <!-- Header -->
-    <header
-      class="bg-[#305d63] text-white flex justify-between items-center p-4"
-    >
-      <div class="flex items-center">
-        <div
-          class="w-10 h-10 bg-gray-300 rounded-full mr-4"
-          @click="navigateTo('/dashboard/account')"
-        ></div>
-        <h1 class="text-xl" @click="navigateTo('/dashboard')">Vaults</h1>
+  <ClientOnly>
+    <body class="h-screen flex flex-col">
+      <!-- Header -->
+      <header
+        class="bg-[#305d63] text-white flex justify-between items-center p-4"
+      >
+        <div class="flex items-center">
+          <div
+            class="w-10 h-10 bg-gray-300 rounded-full mr-4"
+            @click="navigateTo('/dashboard/account')"
+          ></div>
+          <h1 class="text-xl" @click="navigateTo('/dashboard')">Vaults</h1>
+        </div>
+        <LogOutButton />
+      </header>
+
+      <!-- Main Content -->
+      <div class="flex flex-1">
+        <!-- Sidebar -->
+        <SideBar>
+          <VaultRead />
+          <div class="flex-grow"></div>
+          <UButton
+            block
+            icon="i-heroicons-plus"
+            @click="navigateTo('/dashboard/create')"
+          ></UButton>
+        </SideBar>
+
+        <!-- Main Content Area -->
+        <main class="flex-1 p-4">
+          <slot></slot>
+        </main>
       </div>
-      <LogOutButton />
-    </header>
-
-    <!-- Main Content -->
-    <div class="flex flex-1">
-      <!-- Sidebar -->
-      <SideBar>
-        <VaultRead />
-        <div class="flex-grow"></div>
-        <UButton
-          block
-          icon="i-heroicons-plus"
-          @click="navigateTo('/dashboard/create')"
-        ></UButton>
-      </SideBar>
-
-      <!-- Main Content Area -->
-      <main class="flex-1 p-4">
-        <slot></slot>
-      </main>
-    </div>
-  </body>
+    </body>
+  </ClientOnly>
 </template>
 
 <script lang="ts" setup>
