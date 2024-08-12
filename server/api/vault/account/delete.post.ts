047f1ef4586ae29116555228726ad5d66aaa458f
Madeee1 <madeee@duck.com>
2024-08-12 13:57:54 +0000

    feat: deletion now takes into account user's password

--- server/api/vault/account/delete.post.ts
+++ server/api/vault/account/delete.post.ts
@@ -1,9 +1,12 @@
 import { serverSupabaseUser } from '#supabase/server'
 import { serverSupabaseServiceRole } from '#supabase/server'
+import { serverSupabaseClient } from '#supabase/server'
+import bcrypt from 'bcrypt'
 
 export default defineEventHandler(async (event) => {
   const user = await serverSupabaseUser(event)
   const supabase = serverSupabaseServiceRole(event)
+  const supabaseClient = await serverSupabaseClient(event)
 
   // check user is authenticated
   if (!user) {
@@ -13,6 +16,35 @@ export default defineEventHandler(async (event) => {
     })
   }
 
+  // Get hashed password from database
+  const { data: userPassword, error: errorPassword } = await supabaseClient
+    .from('user')
+    .select('hashed_password')
+    .eq('id', user.id)
+    .single()
+
+  if (errorPassword) {
+    throw createError({
+      statusCode: 500,
+      statusMessage: 'Internal Server Error when getting hashed password',
+    })
+  }
+
+  const { password } = await readBody(event)
+
+  console.log('type of password', typeof password)
+  console.log('type of userPassword', typeof userPassword.hashed_password)
+  // Use bcrypt to compare the password
+  const match = await bcrypt.compare(password, userPassword.hashed_password)
+  console.log('match ', match)
+  console.log(userPassword.hashed_password)
+
+  if (!match) {
+    throw createError({
+      statusCode: 401,
+      statusMessage: 'Wrong password',
+    })
+  }
   const { data, error } = await supabase.auth.admin.deleteUser(user.id, true)
 
   if (error) {
